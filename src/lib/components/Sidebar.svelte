4314f04c49edeb9d580ff83bce68bc1f4b1f4507
KikaOrdaz <45240183+KikaOrdaz@users.noreply.github.com>
2024-04-23 17:58:06 +0100

    Changes to sidebar, [folderId]/+page.svelte not adapted accordingly

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,19 +1,16 @@
 <script lang="ts">
     import { ScrollArea } from "$lib/components/ui/scroll-area/index.js";
 	import * as Card from '$lib/components/ui/card';
-    import PlayFill from "$lib/img/play_fill.svelte";
     import Folder from "$lib/img/folder.svelte";
     import { Separator } from "$lib/components/ui/separator";
     import Pencil from "$lib/img/pencil.svelte"
     import {files} from '$lib/files'
-    import { Video } from 'flowbite-svelte';
-	import { Reload } from "svelte-radix";
 
     //export let folderName = "Animais"
+    //let currentPlaylist = files[0]
     export let playlistId: any;
     export let data: any;
     export let current_sign: number;
-    let currentPlaylist = files[0]
     
 
 
@@ -30,6 +27,28 @@
 
     
     $: videoSrc = getSignById(playlist.signs_id[current_sign]).video;
+
+
+    function changeCurrentSign(sign_id : number){
+        current_sign = sign_id
+        dispatch('currentSignChange', current_sign); // Dispatch event
+    }
+
+    function dispatchCurrentSign() {
+    dispatch('currentSignChange', current_sign);
+  }
+
+  function dispatch(name: string, detail: any) {
+    const event = new CustomEvent(name, {
+      detail,
+      bubbles: true,
+      composed: true,
+    });
+    window.dispatchEvent(event);
+  }
+
+  // Listen for changes to current_sign and dispatch
+  $: dispatchCurrentSign();
 
     // export let playlist : {type: string, icon: any, fileName: string, date: string, anotation: boolean, selected: boolean, videos: {label: string, anotated: boolean}[]};
     // $: annotatedCount = playlist.signs_id.filter((v) => v.anotated).length;
@@ -56,33 +75,37 @@
                 <div class="flex flex-col gap-1 items-center">
 
                     {#if playlist.signs_id[current_sign] == sign}
-                        <Card.Root class="flex flex-col w-60  h-20" style="border: 2px solid #0096FF;">
-                            <div class="flex flex-1 flex-row justify-end pt-2 pe-2">
-                                {#if getSignById(sign).anotated}
-                                    <Pencil />
-                                {/if}
-                            </div>
-
-                            <div class="flex flex-row items-center justify-center sticky">
-                                <!-- <img src={getSignById(sign).video} alt=""/> -->
-                                {getSignById(sign).name}
-                            </div>
-                            <div class="flex flex-1 pb-2"></div>
-                        </Card.Root>
-                    {:else}
-                        <Card.Root class="flex flex-col w-60 h-20">
-                            <div class="flex flex-1 flex-row justify-end pt-2 pe-2">
-                                {#if getSignById(sign).anotated}
-                                    <Pencil />
-                                {/if}
+                        <button>
+                            <Card.Root class="flex flex-col w-60  h-20" style="border: 2px solid #0096FF;">
+                                <div class="flex flex-1 flex-row justify-end pt-2 pe-2">
+                                    {#if getSignById(sign).anotated}
+                                        <Pencil />
+                                    {/if}
                                 </div>
 
-                            <div class="flex flex-row items-center justify-center sticky">
-                                <!-- <img src={getSignById(sign).video} alt=""/> -->
-                                {getSignById(sign).name}
-                            </div>
-                            <div class="flex flex-1 pb-2"></div>
-                        </Card.Root>
+                                <div class="flex flex-row items-center justify-center sticky">
+                                    <!-- <img src={getSignById(sign).video} alt=""/> -->
+                                    {getSignById(sign).name}
+                                </div>
+                                <div class="flex flex-1 pb-2"></div>
+                            </Card.Root>
+                        </button>
+                    {:else}
+                        <button on:click={() => {changeCurrentSign(sign - 1)}}>
+                            <Card.Root class="flex flex-col w-60 h-20">
+                                <div class="flex flex-1 flex-row justify-end pt-2 pe-2">
+                                    {#if getSignById(sign).anotated}
+                                        <Pencil />
+                                    {/if}
+                                    </div>
+
+                                <div class="flex flex-row items-center justify-center sticky">
+                                    <!-- <img src={getSignById(sign).video} alt=""/> -->
+                                    {getSignById(sign).name}
+                                </div>
+                                <div class="flex flex-1 pb-2"></div>
+                            </Card.Root>
+                        </button>
                     {/if}
                     
                 </div>
