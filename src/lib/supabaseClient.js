743e1d2add1cc5a9367f7dcc9eb12eef245169af
KikaOrdaz <45240183+KikaOrdaz@users.noreply.github.com>
2024-08-06 10:37:04 +0100

    supabase url updated

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-	'https://gncgottiijvjagbqpuph.supabase.co',
+	'https://exvhkaqgrwffbtpuoyaf.supabase.co',
 	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImduY2dvdHRpaWp2amFnYnFwdXBoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEzODE0NDcsImV4cCI6MjAyNjk1NzQ0N30.Z9zcq6V20q0dOumuxjm55XUyyK8VzCit4AMShCT9yyM'
 );
